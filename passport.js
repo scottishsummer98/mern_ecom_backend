65bfb543b2d3139726fe9d1c0da208f063fc3ac7
scottishsummer98 <samiremail98@gmail.com>
2023-11-30 19:33:31 +0600

    Social Login Email Unique issue fix

--- passport.js
+++ passport.js
@@ -16,6 +16,7 @@ const googleStrategy = new GoogleStrategy(
       googleId: profile.id,
       email: profile._json.email,
     });
+
     if (user) {
       const token = user.generateJWT();
       const response = {
@@ -24,21 +25,32 @@ const googleStrategy = new GoogleStrategy(
       };
       cb(null, response);
     } else {
-      user = new User({
-        googleId: profile.id,
-        name: profile._json.name,
-        email: profile._json.email,
-      });
-      await user.save();
-      const token = user.generateJWT();
-      const response = {
-        message: "Registration Successful!",
-        token: token,
-      };
-      cb(null, response);
+      user = await User.findOne({ email: profile._json.email });
+
+      if (!user) {
+        user = new User({
+          googleId: profile.id,
+          name: profile._json.name,
+          email: profile._json.email,
+        });
+        await user.save();
+        const token = user.generateJWT();
+        const response = {
+          message: "Registration Successful!",
+          token: token,
+        };
+        cb(null, response);
+      } else {
+        const response = {
+          message: "User already exists!",
+          token: "undefined",
+        };
+        cb(null, response);
+      }
     }
   }
 );
+
 const facebookStrategy = new FacebookStrategy(
   {
     clientID: process.env.FACEBOOK_APP_ID,
@@ -59,18 +71,27 @@ const facebookStrategy = new FacebookStrategy(
       };
       cb(null, response);
     } else {
-      user = new User({
-        facebookId: profile.id,
-        name: profile.displayName,
-        email: profile._json.email,
-      });
-      await user.save();
-      const token = user.generateJWT();
-      const response = {
-        message: "Registration Successful!",
-        token: token,
-      };
-      cb(null, response);
+      user = await User.findOne({ email: profile._json.email });
+      if (!user) {
+        user = new User({
+          facebookId: profile.id,
+          name: profile.displayName,
+          email: profile._json.email,
+        });
+        await user.save();
+        const token = user.generateJWT();
+        const response = {
+          message: "Registration Successful!",
+          token: token,
+        };
+        cb(null, response);
+      } else {
+        const response = {
+          message: "User already exists!",
+          token: "undefined",
+        };
+        cb(null, response);
+      }
     }
   }
 );
@@ -78,3 +99,84 @@ passport.use(googleStrategy);
 passport.use(facebookStrategy);
 
 module.exports = passport;
+
+// const passport = require("passport");
+// const { User } = require("./models/user");
+// const _ = require("lodash");
+
+// const GoogleStrategy = require("passport-google-oauth20").Strategy;
+// const FacebookStrategy = require("passport-facebook").Strategy;
+// const googleStrategy = new GoogleStrategy(
+//   {
+//     clientID: process.env.GOOGLE_CLIENT_ID,
+//     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//     callbackURL: `${process.env.REACT_APP_BACK_END_API_URL}api/auth/google/callback`,
+//     proxy: true,
+//   },
+//   async (accessToken, refreshToken, profile, cb) => {
+//     let user = await User.findOne({
+//       googleId: profile.id,
+//       email: profile._json.email,
+//     });
+//     if (user) {
+//       const token = user.generateJWT();
+//       const response = {
+//         message: "Login Successful!",
+//         token: token,
+//       };
+//       cb(null, response);
+//     } else {
+//       user = new User({
+//         googleId: profile.id,
+//         name: profile._json.name,
+//         email: profile._json.email,
+//       });
+//       await user.save();
+//       const token = user.generateJWT();
+//       const response = {
+//         message: "Registration Successful!",
+//         token: token,
+//       };
+//       cb(null, response);
+//     }
+//   }
+// );
+// const facebookStrategy = new FacebookStrategy(
+//   {
+//     clientID: process.env.FACEBOOK_APP_ID,
+//     clientSecret: process.env.FACEBOOK_APP_SECRET,
+//     callbackURL: `${process.env.REACT_APP_BACK_END_API_URL}api/auth/facebook/callback`,
+//     profileFields: ["id", "displayName", "email"],
+//   },
+//   async (accessToken, refreshToken, profile, cb) => {
+//     let user = await User.findOne({
+//       facebookId: profile.id,
+//       email: profile._json.email,
+//     });
+//     if (user) {
+//       const token = user.generateJWT();
+//       const response = {
+//         message: "Login Successful!",
+//         token: token,
+//       };
+//       cb(null, response);
+//     } else {
+//       user = new User({
+//         facebookId: profile.id,
+//         name: profile.displayName,
+//         email: profile._json.email,
+//       });
+//       await user.save();
+//       const token = user.generateJWT();
+//       const response = {
+//         message: "Registration Successful!",
+//         token: token,
+//       };
+//       cb(null, response);
+//     }
+//   }
+// );
+// passport.use(googleStrategy);
+// passport.use(facebookStrategy);
+
+// module.exports = passport;
